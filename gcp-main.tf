d6b63e3ea802d8bfcb211c3945189d626328c26c
Sebastiao Junior <65188546+s3b4hjr@users.noreply.github.com>
2022-05-28 13:23:06 -0300

    deploy appengine

--- gcp-main.tf
+++ gcp-main.tf
@@ -1,12 +1,12 @@
 resource "google_app_engine_application" "app" {
   project     = "lab-devops-t05"
-  location_id = "us-central"
+  location_id = "us-us-west"
 }
 
 resource "google_artifact_registry_repository" "my-repo" {
   provider = google-beta
 
-  location = "us-central1"
+  location = "us-us-west1"
   repository_id = "labdevops"
   description = "Imagens Docker"
   format = "DOCKER"
